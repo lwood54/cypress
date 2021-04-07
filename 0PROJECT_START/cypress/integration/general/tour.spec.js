a0529d82db4c77e8d9711e7d428386642c95ea98
Logan Wood <lwood3499@gmail.com>
2021-04-07 08:45:00 -0500

    tour: basic commands, auto test writing with studio

--- 0PROJECT_START/cypress/integration/general/tour.spec.js
+++ 0PROJECT_START/cypress/integration/general/tour.spec.js
@@ -1,7 +1,17 @@
+// Useful section in cypress docs:
+// https://docs.cypress.io/guides/references/assertions#Common-Assertions
+// https://docs.cypres.io/api/commands/should.html#Syntax
+// Cypress uses Chai assertions
+// https://www.chaijs.com/api/assert/
+// https://www.chaijs.com/api/bdd/   <-- Chains to use with should
 describe('Make sure site loads', () => {
-  it('Page Loads', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000');
-    // cy.contains('Filter');
+  });
+
+  it('Page Loads', () => {
+    cy.contains('Filter');
+
     cy.get('[data-testid=movies-list-movie')
       .first()
       .then(($movie) => {
@@ -9,6 +19,26 @@ describe('Make sure site loads', () => {
         cy.get('[data-testid=movies-list-movie]').first().click();
         cy.url().should('include', movieUrl);
       });
-    expect(true).to.equal(true);
+  });
+
+  it('Correct number of movies', () => {
+    cy.get('[data-testid=movies-list-movie]').should('have.length', 20);
+  });
+
+  it('Undderstands chainers', () => {
+    cy.get('[data-testid=movies-list-movie]').should('have.length', 20);
+    cy.get('[data-testid=movies-list-movie]').should('exist');
+    cy.get('[data-testid=movies-loading-movie]').should('not.exist');
+    // in cypress.json, add { "experimentatlStudio": true}
+    // click in test browser
+    // click on magic wand icon
+    // click around (link to optional actions at the top)
+    // save
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('input').clear();
+    cy.get('input').type('Justice');
+    cy.get('input').clear();
+    cy.get(':nth-child(5) > [data-testid=movies-list-movie] > img').click();
+    /* ==== End Cypress Studio ==== */
   });
 });
